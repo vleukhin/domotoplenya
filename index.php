57159608517a29539f20031c43445b7d30604726
vleukhin <norad-2013@mail.ru>
2016-09-12 15:24:07 +0300

    header murkup

--- index.php
+++ index.php
@@ -4,14 +4,33 @@
 	<meta charset="UTF-8">
 	<link rel="shortcut icon" href="favicon.ico">
 
-	<script src="/js/bootstrap.min.js"></script>
-	
-	<link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css"/>
-	<link rel="stylesheet" type="text/css" href="/css/bootstrap-theme.min.css"/>
+	<script src="js/jquery-3.1.0.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+
+	<link rel="stylesheet" type="text/css" href="css/style.css"/>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"/>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css"/>
 
 	<title>Центр инженерных систем</title>
 </head>
 <body>
-
+<div id="page">
+	<header>
+		<div class="row">
+			<div class="col-lg-3 logo">
+				<div></div>
+			</div>
+			<div class="col-lg-5 call">
+				<div>Звоните на бесплатный номер</div>
+				<span class="phone">8 800 775 85 94</span>
+			</div>
+			<div class="col-lg-4 contacts">
+				<div>Москва <span class="phone">8 495 255 44 50</span></div>
+				<div>Тула <span class="phone">8 495 255 44 50</span></div>
+				<div>Почта для заявок <span class="phone">info@hotklimat.ru</span></div>
+			</div>
+		</div>
+	</header>
+</div>
 </body>
 </html>
