5a286b844cdbaeb40630002fefd754fe1af6db37
vleukhin <norad-2013@mail.ru>
2016-09-12 14:45:13 +0300

    Index

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="shortcut icon" href="favicon.ico">
+
+	<script src="/js/bootstrap.min.js"></script>
+	
+	<link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css"/>
+	<link rel="stylesheet" type="text/css" href="/css/bootstrap-theme.min.css"/>
+
+	<title>Центр инженерных систем</title>
+</head>
+<body>
+<a href="" class="btn btn-danger">asd</a>
+</body>
+</html>
