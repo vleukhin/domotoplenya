8fe1baea9a06ecf127c1f0cb2d45ade79534049f
vleukhin <norad-2013@mail.ru>
2016-09-14 12:06:09 +0300

    Adaptive styles

--- index.php
+++ index.php
@@ -7,27 +7,37 @@
 	<script src="js/jquery-3.1.0.min.js"></script>
 	<script src="js/bootstrap.min.js"></script>
 
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
 	<link rel="stylesheet" type="text/css" href="css/style.css"/>
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"/>
 	<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css"/>
 
 	<title>Центр инженерных систем</title>
+
+	<script>
+		$(window).on('resize', function(){
+			console.log(window.innerWidth)
+		})
+	</script>
 </head>
 <body>
 <div id="page">
 	<header>
-		<div class="row">
-			<div class="col-sm-4 col-xs-12 logo">
-				<div></div>
-			</div>
-			<div class="col-sm-4 col-xs-12 call">
-				<div>Звоните на бесплатный номер</div>
-				<span class="phone">8 800 775 85 94</span>
-			</div>
-			<div class="col-sm-4 col-xs-12 contacts">
-				<div>Москва <span class="phone">8 495 255 44 50</span></div>
-				<div>Тула <span class="phone">8 495 255 44 50</span></div>
-				<div>Почта для заявок <span class="phone">info@hotklimat.ru</span></div>
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-sm-4 col-xs-12 logo">
+					<div></div>
+				</div>
+				<div class="col-sm-4 col-xs-12 call">
+					<div>Звоните на бесплатный номер</div>
+					<span class="phone">8 800 775 85 94</span>
+				</div>
+				<div class="col-sm-4 col-xs-12 contacts">
+					<div>Москва <span class="phone">8 495 255 44 50</span></div>
+					<div>Тула <span class="phone">8 495 255 44 50</span></div>
+					<div>Почта для заявок <span class="phone">info@hotklimat.ru</span></div>
+				</div>
 			</div>
 		</div>
 	</header>
@@ -45,11 +55,24 @@
 	<div id="slogan">
 		<div class="logo-bg">
 			<div class="heading">
-				Отопление частного дома	под ключ
+				Отопление частного дома под ключ
 			</div>
 			<div class="locations">
 				Москва и область <br>
 				Тула и область
+			</div>
+		</div>
+	</div>
+	<div id="services">
+		<div class="row">
+			<div class="col-xs-12 col-sm-6 list">
+				<ul>
+					<li>Все виды отопления</li>
+					<li>Комплектующие по оптовым ценам</li>
+					<li>Только известные бренды</li>
+					<li>Гарантия на работы</li>
+					<li>Гарантия на оборудование</li>
+				</ul>
 			</div>
 		</div>
 	</div>
