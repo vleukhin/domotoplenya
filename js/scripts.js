4555c9e45bfd88a3f9a55250fb9789283fe375bb
vleukhin <norad-2013@mail.ru>
2016-09-16 13:38:49 +0300

    sticky menu

--- js/scripts.js
+++ js/scripts.js
@@ -7,5 +7,27 @@
 				scrollTop: $( $.attr(this, 'href') ).offset().top
 			}, 500);
 		});
+
+		var $window = $(window);
+		var menu = $('#menu');
+		var offset = menu.offset().top;
+		var header = $('header');
+		var height = menu.height() + 36;
+
+		$window.scroll(function(){
+			var scroll = $window.scrollTop();
+
+			if (scroll >= offset){
+				if (!menu.hasClass('sticky')){
+					menu.addClass('sticky');
+					header.css('margin-bottom', '+' + height + 'px')
+				}
+			}
+			else if (menu.hasClass('sticky')) {
+				menu.removeClass('sticky');
+				header.css('margin-bottom', '')
+
+			}
+		});
 	});
 })(window.jQuery);
