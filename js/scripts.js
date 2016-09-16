691880859e5111e82a5641a337d576560ca07f48
vleukhin <norad-2013@mail.ru>
2016-09-16 13:41:22 +0300

    scroll fix

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,18 @@
 (function ($) {
 	$(document).ready(function () {
-		$(document).on('click', '#menu a', function(event){
-			event.preventDefault();
-
-			$('html, body').animate({
-				scrollTop: $( $.attr(this, 'href') ).offset().top
-			}, 500);
-		});
-
 		var $window = $(window);
 		var menu = $('#menu');
 		var offset = menu.offset().top;
 		var header = $('header');
 		var height = menu.height() + 36;
+
+		$(document).on('click', '#menu a', function(event){
+			event.preventDefault();
+
+			$('html, body').animate({
+				scrollTop: $( $.attr(this, 'href') ).offset().top - menu.height()
+			}, 500);
+		});
 
 		$window.scroll(function(){
 			var scroll = $window.scrollTop();
